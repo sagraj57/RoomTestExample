33eda8cfca4741983c190e9b2d7941baba2dcbfa
Sagar <sagraj57@gmail.com>
2018-09-29 09:15:32 +0530

    More Changes 29Sept

---
+++ app/src/main/java/com/sagar/android/roomtestexample/UserAdapter.java
@@ -0,0 +1,41 @@
+package com.sagar.android.roomtestexample;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    ArrayList<String> users;
+    public UserAdapter(ArrayList<String> users) {
+        this.users = users;
+    }
+
+    @Override
+    public UserAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(UserAdapter.ViewHolder holder, int position) {
+        holder.firstName.setText(users.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView firstName;
+        public ViewHolder(View itemView) {
+        super(itemView);
+        firstName = itemView.findViewById(R.id.firstName_textView);
+        }
+    }
+}
