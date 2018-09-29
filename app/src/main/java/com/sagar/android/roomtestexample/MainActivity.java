7bb470dba0952cb711a51e4ad1929cd4b8e76eb3
Sagar <sagraj57@gmail.com>
2018-09-29 08:56:04 +0530

    More Changes 29Sept

--- app/src/main/java/com/sagar/android/roomtestexample/MainActivity.java
+++ app/src/main/java/com/sagar/android/roomtestexample/MainActivity.java
@@ -4,10 +4,19 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    public ArrayList<String> users;
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
 
     private static final String TAG = "MainActivity";
     FloatingActionButton fab;
@@ -17,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         fab = findViewById(R.id.fab);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //Demo Arraylist for adapter
+        users = new ArrayList<>();
+        for (int i = 0; i<100; i++) {
+            users.add("Sagar # " + i);
+        }
+        adapter = new UserAdapter(users);
+        recyclerView.setAdapter(adapter);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
